51427958a70243039ee6e93f6024d3b99b3961f2
Zhichao Zhang <zzcwoshizz@163.com>
2022-11-18 16:50:47 +0800

    init wait crypto

--- packages/react-dids/src/instance/instance.ts
+++ packages/react-dids/src/instance/instance.ts
@@ -1,14 +1,23 @@
 // Copyright 2021-2022 zcloak authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import { initCrypto } from '@zcloak/crypto';
+
 import { DidManager } from './DidManager';
 import { CredentialDidResolver } from './DidResolver';
 import { Keyring } from './Keyring';
 
-export const resolver = new CredentialDidResolver();
+export let resolver: CredentialDidResolver;
 
-export const keyring = new Keyring();
-keyring.load();
+export let keyring: Keyring;
 
-export const didManager = new DidManager(keyring, resolver);
-didManager.load();
+export let didManager: DidManager;
+
+initCrypto().then(() => {
+  resolver = new CredentialDidResolver();
+  keyring = new Keyring();
+  didManager = new DidManager(keyring, resolver);
+
+  keyring.load();
+  didManager.load();
+});
