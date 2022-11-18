12bf588e8c983c846b2adea1f3a00ed21ae607be
alpers <alpers@163.com>
2022-11-18 18:06:58 +0800

    add ctype request modules

--- packages/react-dids/src/instance/DidResolver.ts
+++ packages/react-dids/src/instance/DidResolver.ts
@@ -3,7 +3,8 @@
 
 import { CType } from '@zcloak/ctype/types';
 import { ArweaveDidResolver } from '@zcloak/did-resolver';
-import { DidDocumentWithProof } from '@zcloak/did-resolver/types';
+import { DidDocumentWithProof, DidUrl } from '@zcloak/did-resolver/types';
+import type { HexString } from '@zcloak/crypto/types';
 
 import { get, post } from '../utils/request';
 
@@ -30,6 +31,31 @@ export class CredentialDidResolver extends ArweaveDidResolver {
 
   async getAttesterCtypes(): Promise<CType[]> {
     const res = await get(`${this.server}/ctype`);
+
+    if (res?.code !== 200) {
+      throw new Error(res?.message);
+    } else {
+      return res.data;
+    }
+  }
+
+  async submitClaimerImportCtype(claimerDidUrl: DidUrl, ctypeHash: HexString) {
+    const res = await get(`${this.server}/claimer/${claimerDidUrl}/ctype/${ctypeHash}/import`);
+    if (res?.code !== 200) {
+      throw new Error(res?.message);
+    }
+  }
+
+  async deleteClaimerImportCtype(claimerDidUrl: DidUrl, ctypeHash: HexString) {
+    const res = await get(`${this.server}/claimer/${claimerDidUrl}/ctype/${ctypeHash}/unimport`);
+
+    if (res?.code !== 200) {
+      throw new Error(res?.message);
+    }
+  }
+
+  async getClaimerCtypes(claimerDidUrl: DidUrl): Promise<CType[]> {
+    const res = await get(`${this.server}/claimer/${claimerDidUrl}/ctype`);
 
     if (res?.code !== 200) {
       throw new Error(res?.message);
