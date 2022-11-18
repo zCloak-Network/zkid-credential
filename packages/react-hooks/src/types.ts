f958296b49ce696f01606722122689818ff18a87
Zhichao Zhang <zzcwoshizz@163.com>
2022-11-18 20:24:54 +0800

    notification

--- packages/react-hooks/src/types.ts
+++ packages/react-hooks/src/types.ts
@@ -1,10 +1,20 @@
 // Copyright 2021-2022 zcloak authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import type { DecryptedMessage } from '@zcloak/message/types';
+import type { DecryptedMessage, Message, MessageType } from '@zcloak/message/types';
 
 export type TaskStatus = 'pending' | 'approved' | 'rejected' | 'revoked';
 
 export interface Task extends DecryptedMessage<'Request_Attestation'> {
   status: TaskStatus;
+}
+
+export type MessageMeta = {
+  isRead: boolean;
+  isPush: boolean;
+  dealStatus?: 'approved' | 'rejected';
+};
+
+export interface MessageWithMeta<T extends MessageType> extends Message<T> {
+  meta: MessageMeta;
 }
