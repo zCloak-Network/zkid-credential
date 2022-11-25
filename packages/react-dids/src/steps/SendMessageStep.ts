2257bee630e41d2370a466bb9de8dfbfb9a3b08e
Zhichao Zhang <zzcwoshizz@163.com>
2022-11-25 23:31:46 +0800

    bump deps

--- packages/react-dids/src/steps/SendMessageStep.ts
+++ packages/react-dids/src/steps/SendMessageStep.ts
@@ -5,6 +5,8 @@ import type { Message, MessageType } from '@zcloak/message/types';
 
 import { assert } from '@polkadot/util';
 
+import { verifyMessageEnvelope, verifyMessageSignature } from '@zcloak/message';
+
 import { resolver } from '../instance';
 
 export async function sendMessage<T extends MessageType>(
@@ -12,6 +14,9 @@ export async function sendMessage<T extends MessageType>(
   reCaptchaToken?: string | null
 ): Promise<void> {
   assert(encryptedMessage, 'Not encrypted message found');
+
+  verifyMessageEnvelope(encryptedMessage);
+  await verifyMessageSignature(encryptedMessage, resolver);
 
   await resolver.postMessage(encryptedMessage, reCaptchaToken ?? undefined);
 }
