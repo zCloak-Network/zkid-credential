d6814c44212faba72aa3b2353bac2d60b0d98b38
Zhichao Zhang <zzcwoshizz@163.com>
2022-12-23 18:23:10 +0800

    feat(claim): remove default attester when create claim
    release-as: 0.9.0

--- packages/page-ctype/src/create/CreateClaim.tsx
+++ packages/page-ctype/src/create/CreateClaim.tsx
@@ -4,7 +4,7 @@
 import type { CType } from '@zcloak/ctype/types';
 import type { AnyJson } from '@zcloak/vc/types';
 
-import React, { useCallback, useMemo, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { Did } from '@zcloak/did';
@@ -20,7 +20,6 @@ import {
 } from '@credential/react-components';
 import { CreateSubject } from '@credential/react-ctype';
 import { InputDid } from '@credential/react-dids';
-import { resolver } from '@credential/react-dids/instance';
 import { useCTypeMetaForAttest, useToggle } from '@credential/react-hooks';
 
 import SubmitClaim from './SubmitClaim';
@@ -32,14 +31,6 @@ function CreateClaim({ ctype }: { ctype: CType }) {
   const navigate = useNavigate();
 
   const ctypeMeta = useCTypeMetaForAttest(ctype.$id);
-
-  const defaultAttester = useMemo(() => {
-    try {
-      return resolver.parseDid(ctype.publisher).did;
-    } catch {}
-
-    return ctype.publisher;
-  }, [ctype.publisher]);
 
   const onDone = useCallback(() => {
     toggleOpen();
@@ -63,7 +54,7 @@ function CreateClaim({ ctype }: { ctype: CType }) {
             <Typography mb={4} textAlign="center" variant="h2">
               Create Claim
             </Typography>
-            <InputDid defaultValue={defaultAttester} label="Attester" onChange={setAttester} />
+            <InputDid label="Attester" onChange={setAttester} />
             <Box mt={2}>
               <CreateSubject onChange={setContents as any} schema={ctype} />
             </Box>
