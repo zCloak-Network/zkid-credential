39d77667d6e8081e572f09de97e00bfa2626d85d
alpers <alpers@163.com>
2023-03-28 10:55:12 +0800

    init hk event

---
+++ packages/page-hk-event/README.md
@@ -0,0 +1,1 @@
+# page-hk-event
---
+++ packages/page-hk-event/src/index.tsx
@@ -0,0 +1,8 @@
+// Copyright 2021-2023 zcloak authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+const HkEvent = () => {
+  return <>hk event</>;
+};
+
+export default HkEvent;
