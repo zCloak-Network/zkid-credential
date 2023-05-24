5e388690dee45a2028ecabb837866935f17c2859
alpers <alpers@163.com>
2023-05-24 13:30:59 +0800

    change introduction

--- packages/page-sbt-event/src/Introduction.tsx
+++ packages/page-sbt-event/src/Introduction.tsx
@@ -28,16 +28,17 @@ const Introduction = () => {
     <>
       <Title value='Introduction' />
       <Typography color='#333' fontSize={18} lineHeight='30px' mt={4}>
-        As blockchain compliance and regulatory mechanisms continue to develop, the demand for personal information in
-        on-chain applications is becoming increasingly strong. How to allow on-chain project parties to understand the
-        compliance status of users without revealing any personal information has become an urgent issue to be resolved
-        in the web3 world.
+        As blockchain compliance and regulatory requirements continue to develop, the demand for personal information in
+        on-chain applications is becoming increasingly strong. It has become an urgent issue to enable on-chain
+        verifiers, e.g. smart contracts, to check the compliance status of users without revealing any personally
+        identifiable information (PII).
       </Typography>
       <Typography color='#333' fontSize={18} lineHeight='30px'>
-        The collaboration between zCloak and Chaintool demonstrates such a possibility, where we put the zk calculation
-        results on the blockchain instead of the KYC information itself. In this way, blockchain project parties can
-        develop their own zk Programs based on their compliance requirements and carry out checks in their applications
-        based on the inspection results provided by trusted third parties.
+        The collaboration between zCloak and Chaintool demonstrates such an example, where we put the zk computation
+        results in the blockchain instead of the PII itself. Verifiers can use the no-code tool from zCloak to develop
+        their own zkPrograms based on the regulation in their region. The zkProgram will be executed using user KYC data
+        in their zkID wallet. The end result will become available in the blockchain as a zk-SBT, proving attributes of
+        a user without disclosing their privacy.
       </Typography>
       <Stack direction='row' mb={6} mt={6} spacing={10}>
         <Item label='SOCIAL' value='1000' />
