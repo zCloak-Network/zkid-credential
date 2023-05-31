daf510b8753d29acf5b91c9be6e6edb8e3032990
linhan <15726044837@163.com>
2023-05-31 20:49:01 +0800

    fix: message ctype filter (#38)
    Co-authored-by: alpers <alpers@163.com>

--- packages/page-sbt-event/src/hooks/useSteps.tsx
+++ packages/page-sbt-event/src/hooks/useSteps.tsx
@@ -46,7 +46,10 @@ const stepsConfig: StepCardProps[] = [
 
 function getMessage(messages: Message<keyof MessageData>[], did: Did): Message<keyof MessageData> | undefined {
   return messages.filter(
-    (message) => message.msgType === 'Send_issuedVC' && message.receiver === did.getKeyUrl('keyAgreement')
+    (message) =>
+      message.msgType === 'Send_issuedVC' &&
+      message.receiver === did.getKeyUrl('keyAgreement') &&
+      message?.ctype === ZKSBT_CTYPE
   )[0];
 }
 
