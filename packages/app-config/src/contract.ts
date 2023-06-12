3e5e883942381904a5002c60b31c35b87fc66aaa
linhan <15726044837@163.com>
2023-06-12 18:11:24 +0800

    feat: add Non-American check program config (#41)
    Co-authored-by: alpers <alpers@163.com>

--- packages/app-config/src/contract.ts
+++ packages/app-config/src/contract.ts
@@ -14,7 +14,7 @@ export const VERIFIER_ADDRESS = '0xC2BADDbf6DCeDA9b68638a9de84063c1E0ee4350';
 
 export const ZKSBT_CTYPE = isRelease
   ? '0x0289711acb57f600850341c7b0c8ab484b004ca8a6471777417cea2b8a88ba7a'
-  : '0xa7b43d9dfcaa0dca9f04933146369641f34307916204715ddd972a7f2f1ed443';
+  : '0x0faa3462b6d45be3ce01dc570d8465035f68f516610b267a7b01d9b895d04351';
 
 export const ETHERSCAN_URL = 'https://goerli-optimism.etherscan.io/tx';
 export const OPENSEA_URL = 'https://testnets.opensea.io';
