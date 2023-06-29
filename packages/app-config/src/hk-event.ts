1f5dfdb59ec86d5b6f3929c51eed7e2cd330720f
linhan <15726044837@163.com>
2023-06-29 13:42:47 +0800

    feat: add shanghai event (#45)
    * add shanghai event
    
    * fix lint
    
    ---------
    
    Co-authored-by: alpers <alpers@163.com>

--- packages/app-config/src/hk-event.ts
+++ packages/app-config/src/hk-event.ts
@@ -5,5 +5,5 @@ import { isRelease } from './isRelease';
 
 export const LOGGED_PREFIX = 'logged_hk_event:';
 export const CTYPE_ID = isRelease
-  ? '0x6c62809700813f4ffb59ea97a31f4d34e206a4233bc2bb75bed1d705c4314930'
+  ? '0x541556a04e6d3deccaa2884ae840596cdfe4de597a8334878181ee76aaee745f'
   : '0x6c62809700813f4ffb59ea97a31f4d34e206a4233bc2bb75bed1d705c4314930';
