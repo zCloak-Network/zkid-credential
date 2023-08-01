738e18c71e20b752c81bf13d7729518e046a0c27
王会 <>
2023-08-01 10:12:15 +0800

    fix optional step layout

--- packages/page-sbt-event/src/OptionalStep.tsx
+++ packages/page-sbt-event/src/OptionalStep.tsx
@@ -1,10 +1,11 @@
 // Copyright 2021-2023 zcloak authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { Box } from '@mui/material';
+import { Box, Stack } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 
 import { IconEth } from '@credential/app-config';
+import IconDefi from '@credential/app-config/icons/IconDefi';
 
 import StepCard, { StepCardProps } from './StepCard';
 import Title from './Title';
@@ -17,13 +18,30 @@ const optional: StepCardProps = {
   isLocked: false
 };
 
+const optionalDefiDemo: StepCardProps = {
+  label: 'Optional',
+  content: 'Try zkID Card usage in test Defi Demo.',
+  Icon: <IconDefi />,
+  title: 'Defi Demo',
+  isLocked: false
+};
+
 const OptionalStep = () => {
   const navigate = useNavigate();
 
   return (
     <Box mt={10}>
       <Title value='Optional' />
-      <StepCard {...optional} onClick={() => navigate('/claimer/did/profile')} />
+      <Stack direction='row' flexWrap='wrap' justifyContent='space-between'>
+        <StepCard {...optional} onClick={() => navigate('/claimer/did/profile')} />
+        <StepCard {...optionalDefiDemo} onClick={() => window.open('#/demo', '_blank')} />
+        <Box
+          sx={{
+            width: 370,
+            height: 360
+          }}
+        />
+      </Stack>
     </Box>
   );
 };
